a9c851ae6c39892b3fd533b7d1ea9d64febf1976
jessicavdberg <80203840+jessicavdberg@users.noreply.github.com>
2021-07-06 18:34:04 +0200

    trying

--- Predicting Poverty/trial and error.R
+++ Predicting Poverty/trial and error.R
@@ -101,7 +101,158 @@ grid.arrange(p_age, p_size, p_hhexp, p_cellphone, p_room , p_val, nrow = 2, top 
 #getting rid of the variables that represent the same variable just as monthly household income. Have three variables that basically say the same thing. I want to get rid of them so that we have better information.
 a <- train[,-17] # get rid of income_pp
 b <- a[,-13] # get rid of monthly salary - check.
-corr_var(b, poverty_level, top=5)
+corr_var(b, poverty_level, top=10)
 
+#################################################################
+# Our poverty_level needs to be a factor.
+# this is to ensure that our machine learning models treat this problem as a classification task
+
+train$poverty_level <- as.factor(train$poverty_level)
+test$poverty_level <- as.factor(test$poverty_level)
+
+
+##################################################
+# Different classification models using caret packages
+# have an imbalanced dataset
+# use a default parameter we will try to optimize during training wull be Kappa in order to boost our performance.
+# This can be done by adding the metric=Kappa argument to our train call
+
+# For performance and speed improvements, we will use a 10 K-fold cross validation to fit our models
+# This can be done using trControl function in caret
+
+install.packages('e1071', dependencies=TRUE)
+library(e1071)
+
+# define models to try
+models <- c("multinom", "lda", "naive_bayes", "svmLinear", "knn", "rpart", "ranger")
+# set CV control for knn, k-folds
+control <- trainControl(method = "cv", number = 10, p = .9) # 10 fold, 10%
+# fit models
+set.seed(1)
+train_models <- lapply(models, function(model){
+    print(model)
+    train(poverty_level ~ ., method = model, data = train, trControl = control, metric = "Kappa")
+})
+names(train_models) <- models
+
+
+# extract elapsed training times
+elapsed <- sapply(train_models, function(object)
+    object$times$everything["elapsed"])
+# extract accuracy from CM in one step without creating a separate predictions vector
+acc = sapply(train_models, function(x){
+    pred = predict(x, newdata = test)
+    cm = confusionMatrix(pred, reference = test$poverty_level)
+    return(cm[["overall"]]["Accuracy"])
+}
+)
+# extract F1 by class
+F1 = sapply(train_models, function(x){
+    pred = predict(x, newdata = test)
+    cm = confusionMatrix(pred, reference = test$poverty_level)
+    return(cm[["byClass"]][ , "F1"])
+}
+)
+# extract macro F1
+F1_M = sapply(train_models, function(x){
+    pred = predict(x, newdata = test)
+    cm = confusionMatrix(pred, reference = test$poverty_level)
+    return(mean(cm[["byClass"]][ , "F1"], na.rm = TRUE))
+}
+)
+# extract weighted F1
+F1_W <- sapply(train_models, function(x){
+    pred = predict(x, newdata = test)
+    cm = confusionMatrix(pred, reference = test$poverty_level)
+    actual = colSums(cm$table)
+    F1 = cm[["byClass"]][ , "F1"]
+    return((sum(actual*F1, na.rm = TRUE))/(sum(actual)))
+}
+)
+
+
+## PUT ALL OF THE ABOVE IN A NICE TABLE  in LATEX
+## Also put in dataframe so that you can display nice graphs.
+
+df <- data.frame(models, elapsed, acc, F1_M, F1_W)
+
+#speed vs accuracy
+plot2 <- ggplot(data=df) +
+    geom_point(mapping = aes(x= elapsed, y = acc, color = models, size = 10)) +
+    geom_text(aes(x= elapsed, y = acc, color = models, label = models), hjust =0.7, vjust= -0.8, nudge_x = 0.1) +
+    labs(y = "Accuracy", x = "Speed", title = "Compaing Speed and Accuracy of Different Models", subtitle = "using training model", caption = "Own calculations") +
+    ggthemes::theme_economist_white() +
+    theme(legend.position = "none")
+plot2
+
+
+#speed vs macroF1
+plot3 <- ggplot(data=df) +
+    geom_point(mapping = aes(x= elapsed, y = F1_M, color = models, size = 10)) +
+    geom_text(aes(x= elapsed, y = F1_M, color = models, label = models), hjust =0.7, vjust= -0.8, nudge_x = 0.1) +
+    labs(y = "Macro F1 Score", x = "Speed", title = "Compaing Speed and the Macro-F1 Score of Different Models", subtitle = "using training model", caption = "Own calculations") +
+    ggthemes::theme_economist_white() +
+    theme(legend.position = "none")
+plot3
+
+#speed and macro weighted
+plot4 <- ggplot(data=df) +
+    geom_point(mapping = aes(x= elapsed, y = F1_W, color = models, size = 10)) +
+    geom_text(aes(x= elapsed, y = F1_W, color = models, label = models), hjust =0.7, vjust= -0.8, nudge_x = 0.1) +
+    labs(y = "Weighted F1 Score", x = "Speed", title = "Compaing Speed and the Weighted-F1 Score of Different Models", subtitle = "using training model", caption = "Own calculations") +
+    ggthemes::theme_economist_white() +
+    theme(legend.position = "none")
+plot4
+
+
+### evaluation of Micro scores
+
+F1 %>% kable(col.names = models) %>% kable_styling()
+
+
+# f2 and f3 scores
+
+F2 <- sapply(train_models, function(x){
+    pred = predict(x, newdata = test)
+    cm = confusionMatrix(pred, reference = test$poverty_level)
+    precision = cm[["byClass"]][ , "Precision"]
+    recall = cm[["byClass"]][ , "Recall"]
+    return((5*precision*recall) / (4*precision + recall))
+}
+)
+# extract F-Beta by class
+F3 <- sapply(train_models, function(x){
+    pred = predict(x, newdata = test)
+    cm = confusionMatrix(pred, reference = test$poverty_level)
+    precision = cm[["byClass"]][ , "Precision"]
+    recall = cm[["byClass"]][ , "Recall"]
+    return((10*precision*recall) / (9*precision + recall))
+}
+)
+
+# Class 1: Food poverty
+
+class1 <- rbind(F1[1, ], F2[1, ], F3[1, ])
+row.names(class1) <-  c("F1", "F2", "F3")
+class1 %>% kable(col.names = models, caption = "F-Beta Scores for the Food Poverty line") %>% kable_styling()
+
+# Class 2: lower bound poverty line
+class2 <- rbind(F1[2, ], F2[2, ], F3[2, ])
+row.names(class2) <-  c("F1", "F2", "F3")
+class2 %>% kable(col.names = models, caption = "F-Beta Scores for the Lower Bound Poverty line") %>% kable_styling()
+
+#Class 3: Upper bound poverty line
+class3 <- rbind(F1[3, ], F2[3, ], F3[3, ])
+row.names(class3) <-  c("F1", "F2", "F3")
+class3 %>% kable(col.names = models, caption = "F-Beta Scores for the Upper Bound Poverty line") %>% kable_styling()
+
+# Class4: non-vulnerable
+class4 <- rbind(F1[4, ], F2[4, ], F3[4, ])
+row.names(class4) <-  c("F1", "F2", "F3")
+class4 %>% kable(col.names = models, caption = "F-Beta Scores for Non-Vulnerable Households") %>% kable_styling()
+
+
+# Feauture Importance
+#plot decision tree
 
 
